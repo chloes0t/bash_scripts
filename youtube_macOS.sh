44bc64bede4085a0877e1f25e228811283bfd57c
Dylan <78044999+dylanz0gt@users.noreply.github.com>
2023-10-30 15:12:56 -0400

    Update youtube_macOS.sh

--- youtube_macOS.sh
+++ youtube_macOS.sh
@@ -4,27 +4,22 @@ PROGRAM="youtube-dl"
 
 if [ -e ~/.usr/local/bin/$PROGRAM ]; then
    read -p "Enter path to the location you want the download to go to: " path_
-   youtube-dl --output $path_
    read -p "Enter URL of the video you want to download: " url
-   youtube-dl -f best $url
-   echo "Download successul!"
+   youtube-dl --output "$path_/%(title)s.%(ext)s" -f best "$url"
+   echo "Download successful!"
 else
-   if [!  -e ~/.usr/local/bin/pip3 ]; then
-   echo "pip3, a requirement for youtube-dl will be installed."
-   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-   python3 get-pip.py
-   python3 -m pip install –upgrade pip
+   if [ ! -e ~/.usr/local/bin/pip3 ]; then
+      echo "pip3, a requirement for youtube-dl, will be installed."
+      curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+      python3 get-pip.py
+      python3 -m pip install --upgrade pip
    else
-	echo "youtube-dl will now be installed:"
-	pip3 install youtube-dl
+      echo "youtube-dl will now be installed."
+      pip3 install youtube-dl
+   fi
+
+   read -p "Enter path to the location you want the download to go to: " path_
+   read -p "Enter URL of the video you want to download: " url
+   youtube-dl --output "$path_/%(title)s.%(ext)s" -f best "$url"
+   echo "Download successful!"
 fi
-
-    read -p "Enter path to the location you want the download to go to: " path_
-    youtube-dl --output $path_
-    read -p "Enter URL of the video you want to download: " url
-    youtube-dl -f best $url
-    echo "Download successul!"
-   
-fi
-
-
