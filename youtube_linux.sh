e5131f2017eb29dd5b47b0fad0fa56134f96e97e
Dylan <78044999+dylanz0gt@users.noreply.github.com>
2023-10-30 15:12:02 -0400

    Update youtube_linux.sh

--- youtube_linux.sh
+++ youtube_linux.sh
@@ -2,17 +2,14 @@
 
 dpkg -s youtube-dl &> /dev/null
 
-if [$? -ne 0]
-   then 
-      echo "youtube-dl has not been installed. It will be installed now."
-      sudo apt-get update
-      sudo apt-get install youtube-dl
-   else
-      read -p "Enter path to the location you want the download to go to: " path_
-      youtube-dl --output $path_
-      read -p "Enter URL of the video you want to download: " url
-      youtube-dl -f best $url
-      echo "Download successul!" 
+if [ $? -ne 0 ]
+then 
+   echo "youtube-dl has not been installed. It will be installed now."
+   sudo apt-get update
+   sudo apt-get install youtube-dl
+else
+   read -p "Enter path to the location you want the download to go to: " path_
+   read -p "Enter URL of the video you want to download: " url
+   youtube-dl --output "$path_/%(title)s.%(ext)s" -f best "$url"
+   echo "Download successful!" 
 fi
-
-
